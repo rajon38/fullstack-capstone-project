28935a648a9328f2c74aaddb7110797f7af24caf
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-10-23 21:58:28 +0600

    Cloud IDE frontend lab 7

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,17 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './LoginPage.css';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function LoginPage() {
-  // Define state variables for email and password
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  // Handle login functionality
-  const handleLogin = async () => {
-    console.log("Inside handleLogin");
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
+  //Step 1 - Task 4
+  const [incorrect, setIncorrect] = useState('');
+  //Step 1 - Task 5
+  const navigate = useNavigate();
+  const bearerToken = sessionStorage.getItem('bearer-token');
+  const { setIsLoggedIn } = useAppContext();
+  //Step 1 - Task 6
+  useEffect(() => {
+      if (sessionStorage.getItem('auth-token')) {
+        navigate('/app')
+      }
+    }, [navigate])
+  const handleLogin = async (e) => {
+      e.preventDefault();
+      //api call
+      const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+          //Step 1 - Task 7
+          method: 'POST',
+          //Step 1 - Task 8
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+        },
+      //Step 1 - Task 9
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        })
+      });
+      //Step 2: Task 1
+      const json = await res.json();
+      console.log('Json',json);
+      if (json.authtoken) {
+          //Step 2: Task 2
+        sessionStorage.setItem('auth-token', json.authtoken);
+        sessionStorage.setItem('name', json.userName);
+        sessionStorage.setItem('email', json.userEmail);
+          //Step 2: Task 3
+        setIsLoggedIn(true);
+          //Step 2: Task 4
+        navigate('/app');
+      } else {
+          //Step 2: Task 5
+        document.getElementById("email").value="";
+        document.getElementById("password").value="";
+        setIncorrect("Wrong password. Try again.");
+        setTimeout(() => {
+          setIncorrect("");
+        }, 2000);
+      }
+    }
 
   return (
     <div className="container mt-5">
@@ -28,7 +76,7 @@ function LoginPage() {
                 className="form-control"
                 placeholder="Enter your email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
               />
             </div>
 
@@ -40,8 +88,9 @@ function LoginPage() {
                 className="form-control"
                 placeholder="Enter your password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
               />
+              <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
             </div>
 
             <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
