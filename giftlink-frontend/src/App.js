8e7d95f105906f8e24c56f80ce5df147d69beaf7
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-11-05 19:17:57 +0600

    error2

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import DetailsPage from './components/DetailsPage/DetailsPage';
-import SearchPage from './components/SearchPage/SearchPage';
+
+const MainPage = React.lazy(() => import('./components/MainPage/MainPage'));
+const LoginPage = React.lazy(() => import('./components/LoginPage/LoginPage'));
+const RegisterPage = React.lazy(() => import('./components/RegisterPage/RegisterPage'));
+const DetailsPage = React.lazy(() => import('./components/DetailsPage/DetailsPage'));
+const SearchPage = React.lazy(() => import('./components/SearchPage/SearchPage'));
 
 function App() {
-  const navigate = useNavigate();
+
   return (
     <>
-        <Navbar/>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/login" element={<LoginPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/product/:productId" element={<DetailsPage />} />
+          <Route path="/app/search" element={<SearchPage />} />
         </Routes>
-        </>
+      </Suspense>
+    </>
   );
 }
 
